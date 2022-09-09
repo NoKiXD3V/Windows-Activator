c539fdbafaa375c65e0d0b990d0681264acc5825
NoKiX <112887401+NoKiXD3V@users.noreply.github.com>
2022-09-09 17:30:05 +0200

    Add files via upload

---
+++ activator.py
@@ -0,0 +1,99 @@
+from pystyle import Colors, Colorate
+import requests, os, sys, re, time, random, os.path, string, subprocess, random, threading, ctypes, shutil
+from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
+from colorama import Fore, Style
+from time import sleep
+
+def main():
+        print(Colorate.Horizontal(Colors.blue_to_purple, """
+
+ █████╗  ██████╗████████╗██╗██╗   ██╗ █████╗ ████████╗ ██████╗ ██████╗ 
+██╔══██╗██╔════╝╚══██╔══╝██║██║   ██║██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗
+███████║██║        ██║   ██║██║   ██║███████║   ██║   ██║   ██║██████╔╝
+██╔══██║██║        ██║   ██║╚██╗ ██╔╝██╔══██║   ██║   ██║   ██║██╔══██╗
+██║  ██║╚██████╗   ██║   ██║ ╚████╔╝ ██║  ██║   ██║   ╚██████╔╝██║  ██║
+╚═╝  ╚═╝ ╚═════╝   ╚═╝   ╚═╝  ╚═══╝  ╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
+                                                                       
+
+                
+# by NoKiXDEV
+# GitHub: Comming Soon...
+
+""", 1))
+
+main()
+
+print(Colorate.Horizontal(Colors.red_to_yellow, """
+═══════════════════════════════════════════════════════════\n
+        [+] Windows Keys:
+
+        [1] Windows Pro
+
+        [2] Windows Pro N
+
+        [3] Windows Home
+
+        [4] Windows Home N
+
+        [5] Windows Enterprise
+
+        [6] Windows Enterprise N
+
+        [7] Windows Education
+
+        [8] Windows Education N
+
+═══════════════════════════════════════════════════════════""", 1))
+
+global choice
+choice = input(f"{Fore.CYAN}> {Style.RESET_ALL}")
+
+
+if choice == "1":
+        print(f"{Fore.CYAN}[Activator] {Fore.GREEN}You Succesfully selected Windows Pro {Style.RESET_ALL}")
+        time.sleep(0.5)
+        subprocess.call([r'Activators\pro.bat'])
+        time.sleep(1)
+        exit(123)
+if choice == "2":
+        print(f"{Fore.CYAN}[Activator] {Fore.GREEN}You Succesfully selected Windows Pro N {Style.RESET_ALL}")
+        time.sleep(0.5)
+        subprocess.call([r'Activators\pron.bat'])
+        time.sleep(1)
+        exit(123)
+if choice == "3":
+        print(f"{Fore.CYAN}[Activator] {Fore.GREEN}You Succesfully selected Windows Home {Style.RESET_ALL}")
+        time.sleep(0.5)
+        subprocess.call([r'Activators\home.bat'])
+        time.sleep(1)
+        exit(123)
+if choice == "4":
+        print(f"{Fore.CYAN}[Activator] {Fore.GREEN}You Succesfully selected Windows Home N {Style.RESET_ALL}")
+        time.sleep(0.5)
+        subprocess.call([r'Activators\homen.bat'])
+        time.sleep(1)
+        exit(123)
+if choice == "5":
+        print(f"{Fore.CYAN}[Activator] {Fore.GREEN}You Succesfully selected Windows Enterprise {Style.RESET_ALL}")
+        time.sleep(0.5)
+        subprocess.call([r'Activators\enterprise.bat'])
+        time.sleep(1)
+        exit(123)
+if choice == "6":
+        print(f"{Fore.CYAN}[Activator] {Fore.GREEN}You Succesfully selected Windows Enterprise N {Style.RESET_ALL}")
+        time.sleep(0.5)
+        subprocess.call([r'Activators\enterprisen.bat'])
+        time.sleep(1)
+        exit(123)
+if choice == "7":
+        print(f"{Fore.CYAN}[Activator] {Fore.GREEN}You Succesfully selected Windows Education {Style.RESET_ALL}")
+        time.sleep(0.5)
+        subprocess.call([r'Activators\education.bat'])
+        time.sleep(1)
+        exit(123)
+if choice == "8":
+        print(f"{Fore.CYAN}[Activator] {Fore.GREEN}You Succesfully selected Windows Education N {Style.RESET_ALL}")
+        time.sleep(0.5)
+        subprocess.call([r'Activators\educationn.bat'])
+        time.sleep(1)
+        exit(123)
